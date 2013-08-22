9c00202e83a9a6676a7d244b050d62d1532feb87
Dan Guo <danguo1113@gmail.com>
2013-08-21 22:43:04 -0700

    Got another algorithm. Need to rando generate game, compare speeds

--- find_nash_eq.py
+++ find_nash_eq.py
@@ -16,10 +16,10 @@ def split_payoffs(payoff_matrix_rows, matrix_dim):
         playerB.append(playerB_row_formatted)
     return playerA, playerB
 
-def find_nash_equilibrium(playerA_narr, playerB_narr, matrix_dim):
+def find_nash_equilibrium1(playerA_narr, playerB_narr, matrix_dim):
     # The algorithm is as such
     # playerA_narr strategies are reflected in rows
-    # For each row, find the greatest elements and look in columns if B has incentive to deviate 
+    # For each row in matrix B, find the greatest payoff for B and look in columns if A has incentive to deviate 
     lst_of_nash_eq = []
     for row in range(matrix_dim):
         row_in_B = playerB_narr[row]
@@ -30,6 +30,25 @@ def find_nash_equilibrium(playerA_narr, playerB_narr, matrix_dim):
             if playerA_narr[row][col] == max_payoff_A:
                 lst_of_nash_eq.append((row,col))
     return lst_of_nash_eq
+
+def find_nash_equilibrium(playerA_narr, playerB_narr, matrix_dim):
+    # matrix A
+    max_in_a = []
+    max_in_b = []
+    for i in range(matrix_dim):
+        col = playerA_narr[:,i]
+        max_payoff_A = max(col)
+        rows_of_max = np.where(col == max_payoff_A)[0]
+        max_in_a += zip(rows_of_max,[i] * len(rows_of_max))
+    for i in range(matrix_dim):
+        row = playerB_narr[i]
+        max_payoff_B = max(row)
+        cols_of_max = np.where(row == max_payoff_B)[0]
+        max_in_b += zip([i] * len(cols_of_max),cols_of_max)
+    return [filter(lambda x: x in max_in_a, max_in_b)]
+                      
+        
+        
     
 def parse_game_file(game_file,player_lst):
     f = open(game_file,'r')
@@ -40,7 +59,7 @@ def parse_game_file(game_file,player_lst):
     # A sample line would be '1,2 3,4 5,3'
     # A's strategic choices are row1, row2...
     # B's strategic choices are col1, col2...
-    
+
     lines = f.read()
     lines_split = lines.split('\n')
     matrix_dim = int(lines_split[0])
@@ -51,7 +70,7 @@ def parse_game_file(game_file,player_lst):
     player_lst.append(playerA_narr)
     player_lst.append(playerB_narr)
     return matrix_dim
-
+    
 
 def main():
     args = sys.argv
@@ -59,7 +78,10 @@ def main():
     player_lst = []
     matrix_dim= parse_game_file(game_file,player_lst)
     lst_of_nash_eq = find_nash_equilibrium(player_lst[0], player_lst[1],matrix_dim)
-    print lst_of_nash_eq
+    print 'Algo1: ' + str(lst_of_nash_eq)
+    lst_of_nash_eq = find_nash_equilibrium1(player_lst[0], player_lst[1],matrix_dim)
+    print 'Algo2: ' + str(lst_of_nash_eq)
+    return 0
     
 
 if __name__ == '__main__':
